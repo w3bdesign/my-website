28a54e576f3e71303bc0cfb03e8da1fc73814505
Joan Gerard <joan.gerard@outlook.com>
2022-05-19 18:15:43 +0200

    testing mobile view

--- src/components/ProjectsMobile/ProjectCardMobile/ProjectCardMobile.tsx
+++ src/components/ProjectsMobile/ProjectCardMobile/ProjectCardMobile.tsx
@@ -28,55 +28,57 @@ const ProjectCardMobile: React.FC<Props> = ({
   screenshots,
 }) => {
   return (
-      <div className="ProjectCard">
-        <div className="project-info">
-          <div>
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <p style={{ marginBottom: 0 }}>Built with:</p>
-            <p style={{ marginTop: 5 }}>{technologies}</p>
-          </div>
-          <div className="project-links">
-            <form className="goToGitHub" action={githubLink}>
-              <input
-                className="goToGitHub-btn"
-                type="submit"
-                value="GitHub repo"
-              />
-            </form>
-            <form
-              className={`goToLive ${
-                liveProjectLink.length ? "" : "goToLive__hidden"
-              }`}
-              action={liveProjectLink}
-            >
-              <input className="goToLive-btn" type="submit" value="See live" />
-            </form>
-          </div>
+    <div className="ProjectCard">
+      <div className="project-info">
+        <div>
+          <h2>{name}</h2>
+          <p>{description}</p>
+          <p style={{ marginBottom: 0 }}>Built with:</p>
+          <p style={{ marginTop: 5 }}>{technologies}</p>
         </div>
-        {/* <div className="carouselContainer"> */}
-        <CarouselProvider
-          naturalSlideWidth={1}
-          naturalSlideHeight={1}
-          totalSlides={screenshots.length}
-          visibleSlides={1}
-        >
-          <Slider className="slider">
-            {screenshots.map((screen) => (
-              <img className="screenshot" src={screen} />
-            ))}
-          </Slider>
-          <div className="buttonsContainer">
-            <ButtonBack className="buttonBack">
-              <i className="fa fa-arrow-circle-left fa-3x"></i>
-            </ButtonBack>
-            <ButtonNext className="buttonNext">
-              <i className="fa fa-arrow-circle-right fa-3x"></i>
-            </ButtonNext>
-          </div>
-        </CarouselProvider>
-        {/* </div> */}
+        <div className="project-links">
+          <form className="goToGitHub" action={githubLink}>
+            <input
+              className="goToGitHub-btn"
+              type="submit"
+              value="GitHub repo"
+            />
+          </form>
+          <form
+            className={`goToLive ${
+              liveProjectLink.length ? "" : "goToLive__hidden"
+            }`}
+            action={liveProjectLink}
+          >
+            <input className="goToLive-btn" type="submit" value="See live" />
+          </form>
+        </div>
       </div>
+      {/* <div className="carouselContainer"> */}
+      <CarouselProvider
+        naturalSlideWidth={1}
+        naturalSlideHeight={1}
+        totalSlides={screenshots.length}
+        visibleSlides={1}
+      >
+        <Slider className="slider">
+          {screenshots.map((screen) => (
+            <div className="test">
+              <img className="screenshot" src={screen} />
+            </div>
+          ))}
+        </Slider>
+        <div className="buttonsContainer">
+          <ButtonBack className="buttonBack">
+            <i className="fa fa-arrow-circle-left fa-3x"></i>
+          </ButtonBack>
+          <ButtonNext className="buttonNext">
+            <i className="fa fa-arrow-circle-right fa-3x"></i>
+          </ButtonNext>
+        </div>
+      </CarouselProvider>
+      {/* </div> */}
+    </div>
   );
 };
 
