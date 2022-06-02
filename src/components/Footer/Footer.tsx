8b29288769e8ef32033cb429429788b6548f6477
Joan Gerard <joan.gerard@outlook.com>
2022-06-02 20:41:57 +0200

    add google analytics tracker and hover style on nav links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div id="Footer" className="Footer">
-      © 2022 • Joan Gerard • All rights reserved
-    </div>
+      <div id="Footer" className="Footer">
+        © 2022 • Joan Gerard • All rights reserved
+      </div>
   );
 };
 
